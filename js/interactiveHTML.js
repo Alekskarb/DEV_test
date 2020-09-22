87b92071f98f1066495ce67ad12d6db3235d5b80
Alekskarb <korbutowski@gmail.com>
2020-09-22 16:11:06 +0300

    popup

--- js/interactiveHTML.js
+++ js/interactiveHTML.js
@@ -1,7 +1,13 @@
-let containers = document.querySelectorAll("img");
-let container = document.querySelector(".works_container");
+let body = document.querySelector(".slider__body__content");
+let slider = document.querySelector(".slider");
 
-function clickImg() {
-    let img = document.querySelectorAll("img");
-    img.forEach(item => item.classList.toggle("show"));
+function clickImg(id) {
+    // let img = document.getElementById(`${id}`);
+    // img.forEach(item => item.classList.toggle("slider"));
+    slider.addEventListener('click', function () {
+        // evt.preventDefault();
+        slider.classList.add("slider__show");
+        body.style.backgroundImage = "url('./img/works/ + `${id}` + _featured_works.jpg')";
+    })
 }
+
