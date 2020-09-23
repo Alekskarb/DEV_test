ef901f8f3df97eb2e62cea7794e4eb160764da6a
Alekskarb <korbutowski@gmail.com>
2020-09-23 15:24:39 +0300

    js

--- js/interactiveHTML.js
+++ js/interactiveHTML.js
@@ -1,13 +1,20 @@
 let body = document.querySelector(".slider__body__content");
-let slider = document.querySelector(".slider");
+let img = document.querySelectorAll("img");
 
-function clickImg(id) {
+function clickImg() {
     // let img = document.getElementById(`${id}`);
     // img.forEach(item => item.classList.toggle("slider"));
-    slider.addEventListener('click', function () {
-        // evt.preventDefault();
-        slider.classList.add("slider__show");
-        body.style.backgroundImage = "url('./img/works/ + `${id}` + _featured_works.jpg')";
-    })
+    img.forEach((item) =>
+        item.addEventListener('click', function (e) {
+            console.log(e.target);
+            let image = document.querySelector(".slider__body__content");
+            image.classList.add();
+                // slider.classList.add("slider__show");
+                // body.style.backgroundImage = `url('./img/works/ + ${id} + _featured_works.jpg')`;
+            }
+        )
+    )
 }
+
+clickImg();
 
