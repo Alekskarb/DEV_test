bf3eb9f3ba3df9e0e7e1917d948f2d27813b1c43
Alekskarb <korbutowski@gmail.com>
2020-09-26 20:34:13 +0300

    popup

--- js/interactiveHTML.js
+++ js/interactiveHTML.js
@@ -1,26 +1,26 @@
 let slider = document.querySelector(".slider__body__content");
 let img = document.querySelectorAll(".image");
 let popup = document.querySelector(".slider");
+let close = document.querySelector(".slider__close");
 
 function clickImg() {
     img.forEach((item) =>
         item.addEventListener('click', function (e) {
-            slider.innerHTML = `<img alt=\"missing\" src=\"./img/works/${item.id}_featured_works.jpg\"/>`;
+            slider.innerHTML = `<img alt="missing" src="./img/works/${item.id}_featured_works.jpg"/>
+                                <div class="slider__close"> X </div>`;
             popup.classList.add("open");
-            console.log(item.id);
+            // console.log(item.id);
             }
         )
     )
 }
 function closeImg() {
-    img.forEach((item) =>
-        item.addEventListener('click', function (e) {
-                slider.innerHTML = `<img alt=\"missing\" src=\"./img/works/${item.id}_featured_works.jpg\"/>`;
-                popup.classList.add("slider__show");
-                console.log(item.id);
+    popup.addEventListener('click', function (e) {
+        console.log(e);
+                popup.classList.remove("open");
             }
-        )
     )
 }
 clickImg();
+closeImg();
 
