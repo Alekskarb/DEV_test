378e637d4e6285f9a006faf41edf44887343b19d
Alekskarb <korbutowski@gmail.com>
2020-10-01 16:02:13 +0300

    popup

--- js/interactiveHTML.js
+++ js/interactiveHTML.js
@@ -4,40 +4,38 @@ let popup = document.querySelector(".slider");
 let close = document.querySelector(".slider__close");
 let buttonsRight = document.querySelector(".slider__arrow-right");
 let buttonsLeft = document.querySelector(".slider__arrow-left");
+let imageID ;
 
 function clickImg() {
     img.forEach((item) =>
         item.addEventListener('click', function (e) {
-
-                slider.innerHTML = `<img class="popup__image" alt="missing" src='${item.src}'/>`;
-                popup.classList.add("open");
+            slider.innerHTML = `<img class="popup__image" id='${item.id}' alt="${item.id}" src='${item.src}'/>`;
+            popup.classList.add("open");
+            // returnID(+item.id);
+            imageID = +item.id;
             }
         )
     )
 }
+function returnID(id) {
+    return id;
+}
+// let imageID = returnID();
 
 function closeImg() {
     close.addEventListener('click', function (e) {
-        if (close) {
             popup.classList.remove("open");
-        }
-            console.log(e.target);
-            console.log(close);
         }
     )
 }
 
-function nextImg() {
+function nextImg(id) {
     buttonsRight.addEventListener('click', function (e) {
-            // if (close) {
-            popup.classList.remove("open");
-            // }
-            console.log(e.target);
-            console.log(close);
+        slider.innerHTML = `<img class="popup__image" alt="${id}" src='./img/works/${id++}_featured_works.jpg'/>`;
         }
     )
 }
 clickImg();
 closeImg();
-nextImg();
+nextImg(imageID);
 
