2fa4a5270e6636483b527ed56f4259f345f0a858
Alekskarb <korbutowski@gmail.com>
2020-10-15 20:48:50 +0300

    goodPopUp

--- js/interactiveHTML.js
+++ js/interactiveHTML.js
@@ -1,5 +1,5 @@
 let slider = document.querySelector(".slider__body__content");
-let img = document.querySelectorAll(".image");
+let img = document.querySelectorAll(".works_image");
 let imgWork = document.querySelectorAll(".team__image");
 let popup = document.querySelector(".slider");
 let man = document.querySelectorAll(".team__block");
@@ -27,23 +27,25 @@ function closeImg() {
 
 closeImg();
 buttonsRight.addEventListener('click', function () {
-        slider.innerHTML = `<img class="popup__image" alt="popup_no_image" src='./img/works/${imageID++}_featured_works.jpg'/>`;
-        if (imageID === 16) {
+        imageID++;
+        if (imageID === 17) {
             imageID = 11;
         }
+        slider.innerHTML = `<img class="popup__image" alt="popup_no_image" src='./img/works/${imageID}_featured_works.jpg'/>`;
     }
 )
 buttonsLeft.addEventListener('click', function () {
-        slider.innerHTML = `<img class="popup__image" alt="popup_no_image" src='./img/works/${imageID--}_featured_works.jpg'/>`;
-        if (imageID === 11) {
+        imageID--;
+        if (imageID === 10) {
             imageID = 16;
         }
+        slider.innerHTML = `<img class="popup__image" alt="popup_no_image" src='./img/works/${imageID}_featured_works.jpg'/>`;
     }
 )
 
 function clickMan() {
     // for (let item of man) {
-        man.innerHTML = `<div class="team_comment-block">
+    man.innerHTML = `<div class="team_comment-block">
         <div class="team_comment-title">AL RAYHAN <span> / UI Designer </span></div>
     <div class="team_comment-text">Lorem Ipsum is not simply is an action designer random text
     It has roots in a piece. </div> <br/>
@@ -54,8 +56,11 @@ function clickMan() {
         <i class="fa fa-envelope-o" aria-hidden="true"></i>
         </div>
         </div>`
-    })})}
+}
 
+man.forEach((person) => {
+    person.addEventListener('click', clickMan(person))
+});
 clickMan();
 // }
-man.forEach((person) => {person.addEventListener('click', clickMan(person))});
+
