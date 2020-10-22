f02c282ca4a772810b03ee734dbc0bb4e715bf65
Alekskarb <korbutowski@gmail.com>
2020-10-22 19:06:26 +0300

    hover

--- js/interactiveHTML.js
+++ js/interactiveHTML.js
@@ -1,8 +1,7 @@
 let slider = document.querySelector(".slider__body__content");
 let img = document.querySelectorAll(".works_image");
-let imgWork = document.querySelectorAll(".team__image");
 let popup = document.querySelector(".slider");
-let man = document.querySelectorAll(".team__block");
+let man = document.querySelector(".team_container");
 let close = document.querySelector(".slider__close");
 let buttonsRight = document.querySelector(".slider__arrow-right");
 let buttonsLeft = document.querySelector(".slider__arrow-left");
@@ -43,28 +42,45 @@ buttonsLeft.addEventListener('click', function () {
     }
 )
 let arrPeople = [
-    {name: "alesia", job: "UI designer", describe: "beautiful girl"},
-    {name: "bob", job: "frontend", describe: "blackman"},
-    {name: "alex", job: "backend", describe: "coolman"},
-    {name: "ales", job: "developer", describe: "beardman"},
-    {name: "olga", job: "graphic designer", describe: "beautiful"},
-    {name: "alesia", job: "designer", describe: "beautiful girl"}
+    {name: "alesia", job: "UI designer", describe: "beautiful girl", src: "./img/team/21_team.jpg"},
+    {name: "bob", job: "frontend", describe: "blackman", src: "./img/team/22_team.jpg"},
+    {name: "alex", job: "backend", describe: "coolman", src: "./img/team/23_team.jpg"},
+    {name: "ales", job: "developer", describe: "beardman", src: "./img/team/24_team.jpg"},
+    {name: "olga", job: "graphic designer", describe: "beautiful", src: "./img/team/25_team.jpg"},
+    {name: "alesia", job: "designer", describe: "beautiful girl", src: "./img/team/26_team.jpg"}
 ]
-function clickMan(e) {
-    man[+e.target.id].innerHTML = `<div class="team_comment-block">
-        <div class="team_comment-title">${arrPeople[+e.target.id].name} <span> ${arrPeople[+e.target.id].job} </span></div>
-    <div class="team_comment-text">${arrPeople[+e.target.id].describe}</div> <br/>
-    <div class="team_comment-contacts">
-        <i class="fa fa-facebook" aria-hidden="true"></i>
-        <i class="fa fa-twitter" aria-hidden="true"></i>
-        <i class="fa fa-dribbble" aria-hidden="true"></i>
-        <i class="fa fa-envelope-o" aria-hidden="true"></i>
-        </div>
+
+// function clickMan(e) {
+//     man[+e.target.id].innerHTML = `<img class="team__image" alt="miss_img" id="0" src="./img/team/21_team.jpg"/></div>
+//     <div class="team_comment-block">
+//        <div class="team_comment-title">${arrPeople[+e.target.id].name}
+//        <span> ${arrPeople[+e.target.id].job} </span>
+//        </div>
+//     <div class="team_comment-text">${arrPeople[+e.target.id].describe}
+//     </div>
+//     <div class="team_comment-contacts">
+//         <i class="fa fa-facebook" aria-hidden="true"></i>
+//         <i class="fa fa-twitter" aria-hidden="true"></i>
+//         <i class="fa fa-dribbble" aria-hidden="true"></i>
+//         <i class="fa fa-envelope-o" aria-hidden="true"></i>
+//         </div>
+//         </div>`
+//     console.log(e);
+// }
+
+let newMan = arrPeople.map((item) => {
+    man.innerHTML = `
+        <img class="team__image" alt="miss_img" src="${arrPeople[item].src}"/>
+        <div class="team_comment-block">
+        <div class="team_comment-title"> ${arrPeople[item].name} <span> ${arrPeople[item].job} </span></div>
+        <div class="team_comment-text">${arrPeople[item].describe}</div> <br/>
+            <div class="team_comment-contacts">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-dribbble" aria-hidden="true"></i>
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+            </div>
         </div>`
-    console.log(e);
-}
-man.forEach((item) => {
-    item.addEventListener('click', clickMan);
-    // console.log(item);
 });
+man.append(newMan);
 
